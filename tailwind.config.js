f71184c47399ea000b919fd6819961d2e6bd25ff
Josh Zeltman <joshua.zeltman@gmail.com>
2020-06-27 19:29:31 -0300

    WIP - initial setup for styling the site

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [],
+  theme: {
+    colors: {
+      blue: {
+        light: '#D1F4FF',
+        medium: '#595E72',
+        dark: '#191D32'
+      },
+      green: {
+        light: '#E0F84F',
+        medium: '#3BBC57',
+        dark: '#455F24'
+      },
+      tan: {
+        light: '#F3FAE1'
+      },
+      transparent: 'transparent'
+    },
+    extend: {
+      fontFamily: {
+        display: ['"DM Serif Display"', 'serif'],
+      },
+      height: {
+        '19': '4.75rem'
+      },
+      spacing: {
+        '21': '5.25rem'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
